62cf88b545000583e387acdab781319529711780
Leoholst <leo.holst@elev.ga.ntig.se>
2020-09-01 14:37:36 +0200

    Vilkor och loopar

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -11,21 +11,55 @@ namespace HelloWorld
             Console.WriteLine("How old are you?");
             var input = Console.ReadLine();
             age = int.Parse(input);
+            for(var i = 0; i <age; i++)
+            {
+                Console.WriteLine("You're not special.");
+            }
 
             var name = "";
             Console.WriteLine("What is your name?");
             input = Console.ReadLine();
             name = input;
             Console.WriteLine("Ah so your name is " + name);
+            switch (name)
+            {
+                case "Leo":
+                    Console.WriteLine("You have a beautiful name!");
+                    break;
 
+                case "Fabian":
+                    Console.WriteLine("I'm sorry for you");
+                    break;
+
+                case "Hugo":
+                    Console.WriteLine("Ugh, ew");
+                    break;
+            }
+
+            
+            var mentalHealth = false;
+            while(mentalHealth == false)
+            {
+                Console.WriteLine("How are you?");
+                input = Console.ReadLine();
+                var mood = input;
+                if (mood == "good" || mood == "fine" || mood == "great" || mood == "amazing")
+                {
+                    mentalHealth = true;
+                }
+            }
+
+            Console.WriteLine("Are you alive?");
+            input = Console.ReadLine();
+            var DeadOrNot = input;
             bool aliveOrNot = true;
             if(age > 100)
             {
-                Console.WriteLine("sorry you're dead...");
+                Console.WriteLine("You suggest that "+ DeadOrNot +". But I'm sorry to tell you, you're dead...");
             }
             else
             {
-                Console.WriteLine("You seem alive...");
+                Console.WriteLine(DeadOrNot + " You seem alive...");
             }
 
         }
---
+++ Labb1/Labb1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Labb1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static void age()
+        {
+            int myAge = 18;
+            Console.WriteLine(myAge);
+        }
+
+        static void calculator()
+        {
+
+        }
+    }
+}
