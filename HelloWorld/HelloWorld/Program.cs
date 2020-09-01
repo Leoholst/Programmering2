51c8d53ebb24823571a801503db64d3f60337b22
Leoholst <leo.holst@elev.ga.ntig.se>
2020-09-01 12:03:25 +0200

    Variabler och datatyper läxa

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -7,6 +7,27 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Niklas!");
+            var age = 0;
+            Console.WriteLine("How old are you?");
+            var input = Console.ReadLine();
+            age = int.Parse(input);
+
+            var name = "";
+            Console.WriteLine("What is your name?");
+            input = Console.ReadLine();
+            name = input;
+            Console.WriteLine("Ah so your name is " + name);
+
+            bool aliveOrNot = true;
+            if(age > 100)
+            {
+                Console.WriteLine("sorry you're dead...");
+            }
+            else
+            {
+                Console.WriteLine("You seem alive...");
+            }
+
         }
     }
 }
