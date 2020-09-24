d43e88418858a8ef41710d2e6c605cbd3aed7e2c
Leoholst <leo.holst@elev.ga.ntig.se>
2020-09-24 11:24:37 +0200

    kopierade över main klassen från min andra fil som låg i fel repo

--- Laborationer/Lab_1_OOP/Lab_1_OOP/Program.cs
+++ Laborationer/Lab_1_OOP/Lab_1_OOP/Program.cs
@@ -6,7 +6,32 @@ namespace Lab_1_OOP
     {
         static void Main(string[] args)
         {
-            
+            GreetCustomer();
+            ChooseProduct();
+        }
+
+        static void GreetCustomer()
+        {
+            Customer firstCustomer = new Customer();
+            Console.WriteLine("Hello good sir! What is your name?");
+            var inputName = Console.ReadLine();
+            firstCustomer.customer_Name = inputName;
+            Console.WriteLine("Greetings " + firstCustomer.customer_Name);
+        }
+
+        static void ChooseProduct()
+        {
+            Product chooseYourCar = new Product();
+            Console.WriteLine("What is your dream car model?");
+            var inputModel = Console.ReadLine();
+            chooseYourCar.car_Model = inputModel;
+            Console.WriteLine("Then what is your dream color of your car?");
+            var inputColor = Console.ReadLine();
+            chooseYourCar.car_Color = inputColor;
+            Console.WriteLine("What is your dream car year of production?");
+            var inputYear = Console.ReadLine();
+            chooseYourCar.car_Year = int.Parse(inputYear);
+            Console.WriteLine("So, you want a " + chooseYourCar.car_Year + " " + chooseYourCar.car_Color + " " + chooseYourCar.car_Model + "?");
         }
     }
 }
