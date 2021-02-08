4d4bef7627bfb07386eb7bd7dfd132bba36023f8
Leoholst <leo.holst@elev.ga.ntig.se>
2021-02-08 19:32:03 +0100

    first commit, created classes

---
+++ Laborationer/StrategyPattern/StrategyPattern/StrategyPattern/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace StrategyPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Thank you for choosing Volvo, what model do you want?");
+            
+        }
+    }
+}
