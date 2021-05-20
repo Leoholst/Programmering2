654625aa74c861947cbffb9588ceb3192416ff75
Leoholst <leo.holst@elev.ga.ntig.se>
2021-05-20 10:13:48 +0200

    Kopierat in koden från E-projektet.

--- SlutprojektUtökning/SlutprojektUtökning/SlutprojektUtökning/MainWindow.xaml.cs
+++ SlutprojektUtökning/SlutprojektUtökning/SlutprojektUtökning/MainWindow.xaml.cs
@@ -20,9 +20,225 @@ namespace SlutprojektUtökning
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<int> questionNumbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+        int qNum = 0;
+
+        int i;
+
+        int score;
+
         public MainWindow()
         {
             InitializeComponent();
+            StartGame();
+            NextQuestion();
+        }
+
+        private void checkAnswer(object sender, RoutedEventArgs e)
+        {
+            Button senderButton = sender as Button;
+
+            if (senderButton.Tag.ToString() == "1")
+            {
+                score++;
+            }
+
+            if (qNum < 0)
+            {
+                qNum = 0;
+            }
+            else
+            {
+                qNum++;
+            }
+
+            scoreText.Content = "Answered Correctly " + score + "/" + questionNumbers.Count;
+
+            NextQuestion();
+        }
+
+        private void RestartGame()
+        {
+            score = 0;
+            qNum = -1;
+            i = 0;
+            StartGame();
+        }
+
+        private void NextQuestion()
+        {
+            if (qNum < questionNumbers.Count)
+            {
+                i = questionNumbers[qNum];
+            }
+            else
+            {
+                RestartGame();
+            }
+
+            foreach (var x in myCanvas.Children.OfType<Button>())
+            {
+                x.Tag = "0";
+                x.Background = Brushes.DarkGoldenrod;
+            }
+
+            switch (i)
+            {
+                case 1:
+                    txtQuestion.Text = "Question 1";
+
+                    ans1.Content = "Answer 1";
+                    ans2.Content = "Answer 2 Correct";
+                    ans3.Content = "Answer 3";
+                    ans4.Content = "Answer 4";
+
+                    ans2.Tag = "1";
+
+                    qImage.Source = new BitmapImage(new Uri("./images/1.jpg", UriKind.Relative));
+
+                    break;
+
+                case 2:
+                    txtQuestion.Text = "Question 2";
+
+                    ans1.Content = "Answer 1 Correct";
+                    ans2.Content = "Answer 2";
+                    ans3.Content = "Answer 3";
+                    ans4.Content = "Answer 4";
+
+                    ans1.Tag = "1";
+
+                    qImage.Source = new BitmapImage(new Uri("./images/2.jpg", UriKind.Relative));
+
+                    break;
+
+                case 3:
+                    txtQuestion.Text = "Question 3";
+
+                    ans1.Content = "Answer 1";
+                    ans2.Content = "Answer 2";
+                    ans3.Content = "Answer 3 Correct";
+                    ans4.Content = "Answer 4";
+
+                    ans3.Tag = "1";
+
+                    qImage.Source = new BitmapImage(new Uri("./images/3.jpg", UriKind.Relative));
+
+                    break;
+
+                case 4:
+                    txtQuestion.Text = "Question 4";
+
+                    ans1.Content = "Answer 1";
+                    ans2.Content = "Answer 2";
+                    ans3.Content = "Answer 3";
+                    ans4.Content = "Answer 4 Correct";
+
+                    ans4.Tag = "1";
+
+                    qImage.Source = new BitmapImage(new Uri("./images/4.jpg", UriKind.Relative));
+
+                    break;
+
+                case 5:
+                    txtQuestion.Text = "Question 5";
+
+                    ans1.Content = "Answer 1 Correct";
+                    ans2.Content = "Answer 2";
+                    ans3.Content = "Answer 3";
+                    ans4.Content = "Answer 4";
+
+                    ans1.Tag = "1";
+
+                    qImage.Source = new BitmapImage(new Uri("./images/5.jpg", UriKind.Relative));
+
+                    break;
+
+                case 6:
+                    txtQuestion.Text = "Question 6";
+
+                    ans1.Content = "Answer 1";
+                    ans2.Content = "Answer 2 Correct";
+                    ans3.Content = "Answer 3";
+                    ans4.Content = "Answer 4";
+
+                    ans2.Tag = "1";
+
+                    qImage.Source = new BitmapImage(new Uri("./images/6.jpg", UriKind.Relative));
+
+                    break;
+
+                case 7:
+                    txtQuestion.Text = "Question 7";
+
+                    ans1.Content = "Answer 1";
+                    ans2.Content = "Answer 2";
+                    ans3.Content = "Answer 3 Correct";
+                    ans4.Content = "Answer 4";
+
+                    ans3.Tag = "1";
+
+                    qImage.Source = new BitmapImage(new Uri("./images/7.jpg", UriKind.Relative));
+
+                    break;
+
+                case 8:
+                    txtQuestion.Text = "Question 8";
+
+                    ans1.Content = "Answer 1";
+                    ans2.Content = "Answer 2";
+                    ans3.Content = "Answer 3";
+                    ans4.Content = "Answer 4 Correct";
+
+                    ans4.Tag = "1";
+
+                    qImage.Source = new BitmapImage(new Uri("./images/8.jpg", UriKind.Relative));
+
+                    break;
+
+                case 9:
+                    txtQuestion.Text = "Question 9";
+
+                    ans1.Content = "Answer 1 Correct";
+                    ans2.Content = "Answer 2";
+                    ans3.Content = "Answer 3";
+                    ans4.Content = "Answer 4";
+
+                    ans1.Tag = "1";
+
+                    qImage.Source = new BitmapImage(new Uri("./images/9.jpg", UriKind.Relative));
+
+                    break;
+
+                case 10:
+                    txtQuestion.Text = "Question 10";
+
+                    ans1.Content = "Answer 1";
+                    ans2.Content = "Answer 2";
+                    ans3.Content = "Answer 3";
+                    ans4.Content = "Answer 4 Correct";
+
+                    ans4.Tag = "1";
+
+                    qImage.Source = new BitmapImage(new Uri("./images/10.jpg", UriKind.Relative));
+
+                    break;
+            }
+        }
+
+        private void StartGame()
+        {
+            var randomList = questionNumbers.OrderBy(a => Guid.NewGuid()).ToList();
+
+            questionNumbers = randomList;
+
+            questionOrder.Content = null;
+
+            for (int i = 0; i < questionNumbers.Count; i++)
+            {
+                questionOrder.Content += " " + questionNumbers[i].ToString();
+            }
         }
     }
 }
